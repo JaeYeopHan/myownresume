cdfbe88821245383997bbad4025d20e25b1a6c4c
JaeYeopHan <ljyhanll@gmail.com>
2017-09-01 15:45:50 +0900

    chore: Add utils about dom

---
+++ src/utils/DOM.js
@@ -0,0 +1,15 @@
+const DOM = {
+	render: (root, template, value) => {
+		const wrapper = document.createElement("DIV");
+
+		wrapper.innerHTML = template;
+		root.appendChild(wrapper);
+
+		if (value) {
+			return document.querySelector(value);
+		}
+		return wrapper;
+	},
+};
+
+export default DOM;
