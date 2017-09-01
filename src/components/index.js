17b98b584bcb939cff54088f2de9d1613ab65c8f
JaeYeopHan <ljyhanll@gmail.com>
2017-09-01 15:23:55 +0900

    chore: Scaffold component directory

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import {
+
+} from "./profile";
+
+import {
+
+} from "./work";
+
+import {
+
+} from "./project";
+
+import {
+
+} from "./technique";
+
+import {
+
+} from "./study";
+
+export {
+
+};
