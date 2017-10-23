6e5d8fc35f4a9c0c059ca115fba666223faad9d2
jbee.han <jbee.han@navercorp.com>
2017-10-23 23:06:43 +0900

    refactor(component): Fix render image logic

--- src/themes/Component.js
+++ src/themes/Component.js
@@ -36,13 +36,24 @@ class Component {
 				condition: ({value}) => Array.isArray(value),
 				callback: ({key, value}) => {
 					value.forEach(({logo}, idx) => {
-						Dom.elm(`#${key}_logo_${idx}`).style.backgroundImage = `url("/images/${key}/${logo}")`;
+						let imageUrl;
+
+						if (logo === "") {
+							imageUrl = `url("/images/sudo/default_image_1.png")`;
+						} else {
+							imageUrl = `url("/images/${key}/${logo}")`;
+						}
+						Dom.elm(`#${key}_logo_${idx}`).style.backgroundImage = imageUrl;
 					});
 				},
 			},
 			{
 				condition: ({value}) => !Array.isArray(value),
 				callback: ({key, value}) => {
+					if (value.image === "") {
+						Dom.elm(`#${key}_image`).style.backgroundImage = `url("/images/sudo/default_image_1.png")`;
+						return;
+					}
 					Dom.elm(`#${key}_image`).style.backgroundImage = `url("/images/${key}/${value.image}")`;
 				},
 			},
